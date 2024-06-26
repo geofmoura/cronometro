dc89fcff522618372b4e4e4c32b5782a416976a7
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2024-06-26 00:01:09 -0300

    implementando Js e correções

---
+++ src/javascript/script.js
@@ -0,0 +1,14 @@
+const timerEl = document.getElementById('timer');
+const marksList = document.getElementById('marks-list');
+let intervalId = 0;
+let timer = 0;
+let marks = [];
+
+const formatTime = (time) => {
+    const hours = Math.floor(time / 360000);
+    const minutes = Math.floor((time % 360000) / 6000);
+    const seconds = Math.floor((time % 6000) / 100);
+    const hundredths = time % 100;
+
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${hundredths.toString().padStart(2, '0')}`;
+}
